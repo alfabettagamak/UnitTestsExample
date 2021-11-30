d2627f796830d5d41870f3d2498258005f57d6d1
alfabettagamak <alisa.skrynko@gmail.com>
2021-11-30 21:17:28 +0300

    my first commit

---
+++ UnitTestsExample.App/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace UnitTestsExample.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string textFromConsole;
+            do
+            {
+                Console.WriteLine("Введите что-нибудь");
+                textFromConsole = Console.ReadLine();
+            } while (SimpleClass.IsEmpty(textFromConsole));
+
+            if (textFromConsole == "ololo")
+            {
+                Console.WriteLine("Вы молодец. Мы рассчитаем для вас сумму двух чисел");
+                var a = SimpleClass.InputAndCheckStingForAddition("Введите первое число");
+                var b = SimpleClass.InputAndCheckStingForAddition("Введите второе число");
+                var c = int.Parse(a) + int.Parse(b);
+                Console.WriteLine("Сумма двух чисел = " + c);
+            }
+            else
+            {
+                Console.WriteLine("Вы ввели " + textFromConsole);
+            }
+        }
+    }
+}
---
+++ UnitTestsExample.App/SimpleClass.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace UnitTestsExample.App
+{
+    public class SimpleClass
+    {
+        public static bool IsEmpty(string text)
+        {
+            if (string.IsNullOrEmpty(text))
+            {
+                Console.WriteLine("Вы ничего не ввели");
+                return true;
+            }
+            return false;
+        }
+
+        public static bool IsDigitOnly(string text)
+        {
+            foreach (char t in text)
+            {
+                if (t < '0' || t > '9')
+                {
+                    Console.WriteLine("Вы ввели не цифры, как мы будем их складывать?");
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public static bool IsLengthLessMaxIntLength(string text)
+        {
+            var intLength = int.MaxValue.ToString().Length;
+            if (text.Length >= intLength - 1)
+            {
+                Console.WriteLine($"Вы ввели очень большое значение. Больше, чем {intLength - 1}");
+                return false;
+            }
+            return true;
+        }
+
+        public static string InputAndCheckStingForAddition(string message)
+        {
+            string a;
+            do
+            {
+                Console.WriteLine(message);
+                a = Console.ReadLine();
+            } while (IsEmpty(a) || !IsDigitOnly(a) || !IsLengthLessMaxIntLength(a));
+            return a;
+        }
+    }
+}
